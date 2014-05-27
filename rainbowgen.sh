80929ca01411f03d9d74f51018e9bc737c44c4dc
Celine d'Eon <opfunk@gmail.com>
2014-05-27 03:27:10 -0400

    tweak whitespace

--- rainbowgen.sh
+++ rainbowgen.sh
@@ -8,17 +8,27 @@
 # requires openssl
 
 if [[ -f $1 ]]; then 
+
     wordlist="$1"
+
 elif [[ -f $2 ]]; then
+
     wordlist="$2"
+
 elif [[ $# == 1 ]] && [[ $1 == "-sql" ]]; then
+
     read -p "Enter path to wordlist file: " wordlist    
+
 elif [[ $# == 0 ]] || [[ ! -f $@ ]]; then
+
     read -p "Enter path to wordlist file: " wordlist
+
 elif [[ $# > 2 ]]; then 
+
     printf "%s\n" "Only one argument allowed!"
     printf "%s\n" "Usage: ./rainbowgen.sh [/path/to/wordlist.txt] [-sql]"
     exit 0;
+
 fi
 
 outfile="${wordlist%.*}-hashed"
@@ -40,14 +50,16 @@ if [[ $2 == "-sql" ]] || [[ $1 == "-sql"  ]]; then
     printf "%s\n" 'INSERT INTO rainbow (word, md5hash, sha1hash) VALUES' >> "${outfile}.sql"
     
     for i in $words; do
-	    printf "%s" "('" >> "${outfile}.sql"
-	    printf "%s" "$1" | sed s/"'"/"''"/g >> "${outfile}.sql"
-	    printf "%s" "', '" >> "${outfile}.sql"
-	    printf "%s" "$1" | openssl md5 | awk '{ printf "%s", $2 }' >> "${outfile}.sql"
-	    printf "%s" "', '" >> "${outfile}.sql"
-	    printf "%s" "$1" | openssl sha1 | awk '{ printf "%s", $2 }' >> "${outfile}.sql"
-	    printf "%s" "')," >> "${outfile}.sql"
-	    printf "\n" >> "${outfile}.sql"
+
+	printf "%s" "('" >> "${outfile}.sql"
+	printf "%s" "$1" | sed s/"'"/"''"/g >> "${outfile}.sql"
+	printf "%s" "', '" >> "${outfile}.sql"
+	printf "%s" "$1" | openssl md5 | awk '{ printf "%s", $2 }' >> "${outfile}.sql"
+	printf "%s" "', '" >> "${outfile}.sql"
+	printf "%s" "$1" | openssl sha1 | awk '{ printf "%s", $2 }' >> "${outfile}.sql"
+	printf "%s" "')," >> "${outfile}.sql"
+	printf "\n" >> "${outfile}.sql"
+
     done
     
     printf "%s\n" "('end of table!', 'insert statement automated by', 'ur boi d');" >> "${outfile}.sql"
@@ -60,10 +72,12 @@ else
     cat /dev/null > "${outfile}.txt"
 
     for i in $words; do
-	    printf "%s" "$i " >> "${outfile}.txt"
-	    printf "%s" "$i" | openssl md5 | awk '{ printf "%s", $2 }' >> "${outfile}.txt"
-	    printf "%s" " " >> "${outfile}.txt"
-	    printf "%s" "$i" | openssl sha1 | awk '{ print $2 }' >> "${outfile}.txt"
+	    
+	printf "%s" "$i " >> "${outfile}.txt"
+	printf "%s" "$i" | openssl md5 | awk '{ printf "%s", $2 }' >> "${outfile}.txt"
+	printf "%s" " " >> "${outfile}.txt"
+	printf "%s" "$i" | openssl sha1 | awk '{ print $2 }' >> "${outfile}.txt"
+    
     done
 
     printf "%s\n" "DONE! check ${outfile}.txt in current directory for the generated list."
